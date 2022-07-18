2090362e0215e61ab0ed07c3e884fefe215a7380
Romário Ferreira <romarioffranca@gmail.com>
2022-07-18 16:11:03 -0300

    Project hr-worker created

---
+++ hr-worker/src/main/java/com/francaemp/hrworker/HrWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.francaemp.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+}
