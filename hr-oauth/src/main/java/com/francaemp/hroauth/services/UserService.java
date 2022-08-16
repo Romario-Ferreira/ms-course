7954ea82b2feb283d803d72d251a60de9652fbec
Romário Ferreira <romarioffranca@gmail.com>
2022-08-16 14:42:16 -0300

    updates

--- hr-oauth/src/main/java/com/francaemp/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/francaemp/hroauth/services/UserService.java
@@ -5,6 +5,9 @@ package com.francaemp.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.francaemp.hroauth.entities.User;
@@ -12,7 +15,7 @@ import com.francaemp.hroauth.feignclients.UserFeignClient;
 
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 
 	private static Logger log = LoggerFactory.getLogger(UserService.class);
 	
@@ -26,6 +29,17 @@ public class UserService {
 			throw new IllegalArgumentException("Email not found");
 		}
 		log.info("email found: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			log.error("Email not found" + username);
+			throw new UsernameNotFoundException("Username not found");
+		}
+		log.info("email found: " + username);
 		return user;
 	}
 }
